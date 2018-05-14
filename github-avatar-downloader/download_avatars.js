1f8cf5f88b4981e2069fbf6fb8f7f5b54c300a13
Jin <hjd007wwkd@hotmail.com>
2018-05-14 20:36:58 +0000

    v2

--- github-avatar-downloader/download_avatars.js
+++ github-avatar-downloader/download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require("fs");
 var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -17,13 +18,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on("error", function(err){
+    console.log(err);
+  })
+  .pipe(fs.createWriteStream(filePath));
+}
+
 getRepoContributors("jquery", "jquery", function(err, result){
   if(err){
     console.log(err);
   } else {
     var resultJ = JSON.parse(result);
     resultJ.forEach(function(item){
-      console.log(item.avatar_url)
+      downloadImageByURL(item.avatar_url, "avatars/"+item.login+".jpg")
     });
   }
 });
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+
