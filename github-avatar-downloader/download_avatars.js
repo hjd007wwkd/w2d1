bac16bcc710b13789b3a0f857bf02f9d7b0dad2f
Jin <hjd007wwkd@hotmail.com>
2018-05-15 01:59:04 +0000

    v5

--- github-avatar-downloader/download_avatars.js
+++ github-avatar-downloader/download_avatars.js
@@ -4,6 +4,13 @@ var fs = require("fs");
 var dotenv = require('dotenv').config();
 var secrets = require('./secrets');
 
+
+// if (dotenv && dotenv.error && dotenv.error.code === 'ENOENT') {
+//   console.log("fuck fuck fuck fuck fuck!!!!!!!!!");
+//   throw "FUCK!!"
+// }
+
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -16,8 +23,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    console.log(res.headers);
-    cb(err, body);
+    if(res.statusCode === 404) {
+      console.log("the provided owner/repo does not exist");
+    } else if(process.env.GITHUB_TOKEN === undefined){
+      console.log("missing authorized informations: .env file should exist, and should contain GITHUB_TOKEN key");
+    } else if(res.statusCode === 401){
+      console.log("you are unautorized");
+    } else if (!fs.existsSync('avatars/')) {
+        console.log("there is no avatars/ directory");
+    } else {
+      cb(err, body);
+    }
   });
 }
 
@@ -26,7 +42,10 @@ function downloadImageByURL(url, filePath) {
   .on("error", function(err){
     console.log(err);
   })
-  .pipe(fs.createWriteStream(filePath));
+  .pipe(fs.createWriteStream(filePath))
+  .on('finish', function(err){
+    console.log("download completed")
+  })
 }
 
 if(args.length === 4){
---
+++ recommended_repos/recommend.js
@@ -0,0 +1,80 @@
+var args = process.argv;
+var request = require('request');
+var fs = require("fs");
+var dotenv = require('dotenv').config();
+var secrets = require('./secrets');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+var star = {};
+var contribute = 0;
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + process.env.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    if(res.statusCode === 404){
+      console.log("the provided owner/repo does not exist");
+    } else if(process.env.GITHUB_TOKEN === undefined){
+      console.log("missing authorized informations");
+    } else if(res.statusCode === 401){
+      console.log("you are unautorized");
+    }
+    else {
+      cb(err, body);
+    }
+  });
+}
+function getStarred(item){
+  var starredOption = {
+    url: "https://api.github.com/users/"+item.login+"/starred",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + process.env.GITHUB_TOKEN
+    }
+  };
+  request(starredOption, function(err, res, body){
+    if(err){
+      console.log(err);
+    } else {
+      var starredPeople = JSON.parse(body);
+      starredPeople.forEach(function(person){
+        if(star[person.full_name] === undefined) {
+          star[person.full_name] = 1;
+        } else {
+          star[person.full_name]++;
+        }
+      })
+      contribute--;
+      if(contribute === 0 ){
+        console.log(star);
+      }
+    }
+    // count one finished
+  })
+}
+
+if(args.length === 4){
+  getRepoContributors(args[2], args[3], function(err, result){
+    if(err){
+      console.log(err);
+    } else {
+      var contributors = JSON.parse(result);
+      contribute = contributors.length
+      // remember how many will start  ( resultj.length )
+      contributors.forEach(function(item){
+        getStarred(item)
+      });
+    }
+  });
+} else {
+  console.log("Invalid input!!");
+}
+
+
+
